1e8ba292ec968f128121763bd3933a1d86dd74eb
Observer234 <rudolf.chamberlain@gmail.com>
2021-11-14 20:47:29 +0100

    added redux persist and moving directory state into Redux - end of lesson 10-145

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+// to remove firefox warning, assign first before exporting
+const storeContext = { store, persistor }
+
+export default storeContext;
